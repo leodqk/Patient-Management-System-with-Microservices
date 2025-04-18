cb7c76ba4336ff8423f7fba0294770ead3fe1d1e
Dương Quang Khải <94659049+leodqk@users.noreply.github.com>
2025-04-18 22:39:19 +0700

    validate filter with api-gateway

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -9,9 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -38,5 +36,21 @@ public class AuthController {
 
         String token = tokenOptional.get();
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(
+            @RequestHeader("Authorization") String authHeader) {
+
+        // Authorization: Bearer <token>
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
